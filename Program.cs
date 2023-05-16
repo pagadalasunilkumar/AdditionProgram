748df464ba720f67f286459db8de348be7a4fc61
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-16 10:15:31 +0530

    [sunil]-Added test file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine($"Addition of {firstNumber} and {secondNumber} is = {result}");
+            Console.WriteLine("Addition of" + firstNumber + "and" + secondNumber + "is=" + result);
+
+        }
+    }
+}
